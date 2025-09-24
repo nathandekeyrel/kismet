1ed3350904d6be3b7c5e15a58518f7c98ddcac37
nathandekeyrel <natedekeyrel@gmail.com>
2025-09-24 15:55:03 -0600

    init

---
+++ src/main/java/com/github/nathandekeyrel/kismet/KismetApplication.java
@@ -0,0 +1,13 @@
+package com.github.nathandekeyrel.kismet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KismetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KismetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nathandekeyrel/kismet/KismetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nathandekeyrel.kismet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KismetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
