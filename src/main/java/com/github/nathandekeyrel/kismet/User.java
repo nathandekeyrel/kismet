7cc7c52048628d53d4e93ec36f73fd4231743f19
nathandekeyrel <natedekeyrel@gmail.com>
2025-09-24 17:56:51 -0600

    feat(db): add user entity and repository

---
+++ src/main/java/com/github/nathandekeyrel/kismet/User.java
@@ -0,0 +1,21 @@
+package com.github.nathandekeyrel.kismet;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
